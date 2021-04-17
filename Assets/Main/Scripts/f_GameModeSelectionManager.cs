a34538cc3ac14f83b0a19dcd404d50030da7425a
JevX <42956753+JevX@users.noreply.github.com>
2021-04-17 16:28:20 +0500

    Добавлены ассеты Одина и AR помощника, создал новую сцену-дубль и копии скриптов от бэйблейда

---
+++ Assets/Main/Scripts/f_GameModeSelectionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class f_GameModeSelectionManager : MonoBehaviour
+{
+
+
+    #region UI Callback Methods
+    public void OnARModeSelected()
+    {
+        //Setting the gamemode to player custom properties so that Game Mode info can be accessed in all scenes. 
+        //Setting Game mode as AR.
+        ExitGames.Client.Photon.Hashtable playerSelectionProp = new ExitGames.Client.Photon.Hashtable { { MultiplayerARSpinnerTopGame.GAME_MODE, MultiplayerARSpinnerTopGame.GameModeTypes.AR_Mode} };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(playerSelectionProp);
+
+        //Loading the gameplay scene
+        SceneLoader.Instance.LoadScene("Scene_Gameplay");
+
+
+    }
+
+    public void OnNonARModeSelected()
+    {
+        //Setting the gamemode to player custom properties so that Game Mode info can be accessed in all scenes. 
+        //Setting Game mode as Non-AR.
+        ExitGames.Client.Photon.Hashtable playerSelectionProp = new ExitGames.Client.Photon.Hashtable { { MultiplayerARSpinnerTopGame.GAME_MODE, MultiplayerARSpinnerTopGame.GameModeTypes.NonAR_Mode} };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(playerSelectionProp);
+
+        //Loading the gameplay scene
+        SceneLoader.Instance.LoadScene("Scene_Gameplay");
+    }
+
+
+    #endregion
+
+}
